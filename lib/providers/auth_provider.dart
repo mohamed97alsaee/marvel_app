bbc3a6367e45f9ab2576422ed8bdddc7274535c6
Mohamed Alsaee <44772147+mohamed97alsaee@users.noreply.github.com>
2023-09-05 18:33:30 +0200

    Login Task.

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -36,41 +37,7 @@ class AuthProvider with ChangeNotifier {
     }
   }
 
-  Future<bool> login(Map userBody) async {
-    setLoading(true);
-    if (kDebugMode) {
-      print(userBody);
-    }
-    final response = await http.post(
-        Uri.parse("http://172.20.10.9:8000/api/auth/login"),
-        headers: {"Accept": "application/json"},
-        body: json.encode(userBody));
-    //{"email": userBody["email"], "password": userBody["password"]}
-    if (response.statusCode == 200) {
-      var decodedToken = json.decode(response.body)['access_token'];
-      final SharedPreferences prefs = await SharedPreferences.getInstance();
-
-      prefs.setString("token", decodedToken);
-
-      if (kDebugMode) {
-        print("LOGED IN");
-        print("SATUS CODE${response.statusCode}");
-
-        print("BODY${response.body}");
-      }
-    } else {
-      if (kDebugMode) {
-        print("LOGED IN FAILED");
-        print("SATUS CODE${response.statusCode}");
-
-        print("BODY${response.body}");
-      }
-    }
-
-    setLoading(false);
-
-    return true;
-  }
+  Future login(Map<String, dynamic> userBody, BuildContext context) async {}
 
   Future<bool> logout() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -12,80 +12,126 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  TextEditingController emailController = TextEditingController();
+  bool isValidForm = false;
+  validation() async {
+    if (phoneController.text.isNotEmpty &&
+        phoneController.text.length == 9 &&
+        passwordController.text.length >= 8) {
+      isValidForm = true;
+    } else {
+      isValidForm = false;
+    }
+    setState(() {});
+  }
+
+  TextEditingController phoneController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+
   bool hidePass = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed: () async {
-        Provider.of<AuthProvider>(context, listen: false).login({
-          "email": emailController.text.toString(),
-          "password": passwordController.text.toString(),
-        }).then((logedIn) {
-          if (logedIn) {
-            Navigator.pushAndRemoveUntil(
-                context,
-                CupertinoPageRoute(builder: (context) => const ScreenRouter()),
-                (route) => false);
-          }
-        });
-      }),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "Login",
-                style: TextStyle(fontSize: 20),
-              ),
-              TextFormField(
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                controller: emailController,
-                decoration: const InputDecoration(
-                    hintText: "email",
-                    hintStyle: TextStyle(color: Colors.grey)),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "please enter email";
-                  }
-                  if (value.length < 8) {
-                    return "user email must be 8 chars";
-                  }
+          child: Form(
+            // key:
+            // ,
 
-                  return null;
-                },
-              ),
-              TextFormField(
-                obscureText: hidePass,
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                controller: passwordController,
-                decoration: InputDecoration(
-                    suffix: GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            hidePass = !hidePass;
-                          });
-                        },
-                        child: Icon(hidePass
-                            ? Icons.visibility
-                            : Icons.visibility_off)),
-                    hintText: "password",
-                    hintStyle: const TextStyle(color: Colors.grey)),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "please enter password";
-                  }
-                  if (value.length < 8) {
-                    return "password must be 8 chars";
-                  }
+            onChanged: () {
+              print(phoneController.text);
+              validation();
+            },
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Login",
+                  style: TextStyle(fontSize: 20),
+                ),
+                TextFormField(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  controller: phoneController,
+                  decoration: const InputDecoration(
+                      hintText: "email",
+                      hintStyle: TextStyle(color: Colors.grey)),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "please enter email";
+                    }
+                    if (value.length < 8) {
+                      return "user email must be 8 chars";
+                    }
 
-                  return null;
-                },
-              ),
-            ],
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  obscureText: hidePass,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  controller: passwordController,
+                  decoration: InputDecoration(
+                      suffix: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              hidePass = !hidePass;
+                            });
+                          },
+                          child: Icon(hidePass
+                              ? Icons.visibility
+                              : Icons.visibility_off)),
+                      hintText: "password",
+                      hintStyle: const TextStyle(color: Colors.grey)),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "please enter password";
+                    }
+                    if (value.length < 8) {
+                      return "password must be 8 chars";
+                    }
+
+                    return null;
+                  },
+                ),
+                TextButton(
+                    onPressed: () {
+                      if (isValidForm) {
+                        Provider.of<AuthProvider>(context, listen: false)
+                            .login({
+                          "phone": phoneController.text.toString(),
+                          "password": passwordController.text.toString(),
+                          "device_name": "iphone"
+                        }, context).then((logedIn) {
+                          if (logedIn.first) {
+                            Navigator.pushAndRemoveUntil(
+                                context,
+                                CupertinoPageRoute(
+                                    builder: (context) => const ScreenRouter()),
+                                (route) => false);
+                          } else {
+                            SnackBar snackBar = SnackBar(
+                              content: Text(logedIn.last),
+                            );
+
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(snackBar);
+                          }
+                        });
+                      } else {
+                        const snackBar = SnackBar(
+                          content: Text("Fill The data"),
+                        );
+
+                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                      }
+                    },
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                          color: isValidForm ? Colors.blue : Colors.grey),
+                    ))
+              ],
+            ),
           ),
         ),
       ),
